14b20715b8377deaa4215e1984c5d750ff06b8be
j-ohene <jesicaohene1@gmail.com>
2024-01-31 14:46:27 -0500

    functionality

--- js/main.js
+++ js/main.js
@@ -8,12 +8,10 @@
         fetch(`${baseUrl}people/`)
         .then(response =>response.json())
         .then(function(response){
-            //console.log(response.description);
             const characters = response.results;
             const ul = document.createElement('ul');
             characters.forEach(character=>{
-              // console.log(character['name']);
-               //console.log(character['height']);
+          
                 const li = document.createElement('li');
                 const a = document.createElement('a');
                 a.textContent = character['name'];
@@ -32,34 +30,38 @@
         })
         .catch(error=> {
             console.log(error);
-            //ideallly we would write to the DOM and let user know, something is wrong
+        
         });
     
     }
     function getTitle(){
-       // console.log("getReview called");
-       //console.log(e.currentTarget.dataset.review)
-       //console.log(this.dataset);
+  
        fetch(`${baseUrl}films/?format=json`)
-       //https://swapi.dev/api/films/2/?format=json
        .then(response => response.json())
        .then(function(response) {
         filmCon.innerHTML = "";
         const movies = response.results;
-        movies.forEach(movie=>{
-       //console.log(movie['filmTitle']);
+        movies.forEach((movie) =>{
          const template = document.importNode(filmTemplate.content, true);
          const filmBody = template.querySelector(".film-description");
          const filmTitle = template.querySelector(".film-heading");
          filmTitle.innerHTML = movie['title'];
          filmBody.innerHTML = movie ['opening_crawl'];
-
          filmCon.appendChild(template);
-        });
-        
-    })
 
-       .catch();
-    }
+        const index = movies.index(movie);
+         const moviePic = document.createElement("img");
+         moviePic.src = img/(`${index+1}.jpg`);
+            moviePic.addEventListener("click", function(movie){
+    
+            })
+         });
+        filmTitle.appendChild(moviePic);
+
+   })
+   
+    .catch();
+    }        
     getPeople();
+
     })();
