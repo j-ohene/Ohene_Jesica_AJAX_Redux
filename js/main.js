65d3d34c3dc2977794fcf10a7f483ab121992c71
j-ohene <jesicaohene1@gmail.com>
2024-03-19 15:11:07 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,62 +6,94 @@
     
     function getPeople() {
         fetch(`${baseUrl}people/`)
-        .then(response =>response.json())
+        .then(response => response.json())
         .then(function(response){
             const characters = response.results;
             const ul = document.createElement('ul');
             characters.forEach(character=>{
-          
                 const li = document.createElement('li');
                 const a = document.createElement('a');
                 a.textContent = character['name'];
-                a.dataset.review = character['url']
+                // Store character's URL as a data attribute
+                a.dataset.url = character['url'];
                 li.appendChild(a);
                 ul.appendChild(li);
-    
             });
             characterBox.appendChild(ul);
         })
         .then(function(){
+            // Listen for clicks on character links
             const links = document.querySelectorAll('#character-box li a');
             links.forEach(link=> {
                 link.addEventListener("click", getTitle);
-            })
+            });
         })
         .catch(error=> {
             console.log(error);
-        
         });
-    
     }
-    function getTitle(){
-  
-       fetch(`${baseUrl}films/?format=json`)
-       .then(response => response.json())
-       .then(function(response) {
-        filmCon.innerHTML = "";
-        const movies = response.results;
-        movies.forEach((movie) =>{
-         const template = document.importNode(filmTemplate.content, true);
-         const filmBody = template.querySelector(".film-description");
-         const filmTitle = template.querySelector(".film-heading");
-         filmTitle.innerHTML = movie['title'];
-         filmBody.innerHTML = movie ['opening_crawl'];
-         filmCon.appendChild(template);
-
-        const index = movies.index(movie);
-         const moviePic = document.createElement("img");
-         moviePic.src = img/(`${index+1}.jpg`);
-            moviePic.addEventListener("click", function(movie){
     
+    function getTitle(event){
+        const characterUrl = event.target.dataset.url;
+        
+        fetch(characterUrl)
+        .then(response => response.json())
+        .then(function(characterData) {
+            const filmUrls = characterData.films;
+            // Clear previous film data
+            filmCon.innerHTML = "";
+            // Fetch details of each film the character appeared in
+            filmUrls.forEach(filmUrl => {
+                fetch(filmUrl)
+                .then(response => response.json())
+                .then(function(filmData) {
+                    const template = document.importNode(filmTemplate.content, true);
+                    const filmBody = template.querySelector(".film-description");
+                    const filmTitle = template.querySelector(".film-heading");
+                    filmTitle.textContent = filmData.title;
+                    filmBody.textContent = filmData.opening_crawl;
+                    filmCon.appendChild(template);
+                    const moviePic = document.createElement("img");
+                    // Set the source of the image
+                    moviePic.src = `img/${filmData.episode_id}.jpg`;
+                    // Add alt text
+                    moviePic.alt = filmData.title;
+                    // Append the image to the film template
+                    template.appendChild(moviePic);
+
+                    // Create an image element for the movie poster
+                    filmTitle.addEventListener("click", function () {
+                        // Create a container for the movie picture
+                        const moviePictureContainer = document.createElement("div");
+                        moviePictureContainer.classList.add("movie-picture-container");
+
+                        // Create the movie picture element
+                        const moviePicture = document.createElement("img");
+                        moviePicture.src = `img/${filmData.episode_id}.jpg`;
+                        moviePicture.alt = filmData.title;
+
+                        // Append the movie picture to the container
+                        moviePictureContainer.appendChild(moviePicture);
+
+                        // Clear previous movie picture (if any) and display the new one
+                        filmCon.innerHTML = "";
+                        filmCon.appendChild(moviePictureContainer);
+
+
+                })
             })
-         });
-        filmTitle.appendChild(moviePic);
+                .catch(error => {
+                    console.error('Error fetching film data:', error);
+                });
 
-   })
-   
-    .catch();
+
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching character data:', error);
+        });
     }        
-    getPeople();
 
-    })();
+    getPeople();
+})();
+
